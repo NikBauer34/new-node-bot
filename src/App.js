b6891d23d09d6d69a796ed2a66e3788e557ac9c0
NikBauer34 <nik.bauer.09@mail.ru>
2023-01-31 13:24:00 +0300

    init

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       work
-        <button onClick={onClose}>Закрыть</button>
+        <button onClick={onClose}>Зак</button>
     </div>
   );
 }
